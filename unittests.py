3f6612c39c7e68b24becfadb87020621d50fa014
Bryan <bryan.wee@u.yale-nus.edu.sg>
2022-02-14 16:32:19 +0800

    Added Unit Tests, and some documentation for the functions

---
+++ unittests.py
@@ -0,0 +1,30 @@
+from game100222 import (
+    GameState,
+)
+
+
+game = GameState()
+
+
+def test_generateNewWord():
+    try:
+        game.generateNewWord()
+    except NameError:
+        assert False, "generateNewWord not defined"
+
+    testword = game.generateNewWord()
+    assert len(testword) < 39
+    assert len(testword) > 3
+
+
+def test_scoreReset():
+    try:
+        game = GameState()
+        game.scoreReset()
+    except NameError:
+        assert False, "scoreReset not defined"
+
+    game = GameState()
+    game.scoreReset()
+    assert game.score == 0
+    assert game.congratsplayedbefore == False
